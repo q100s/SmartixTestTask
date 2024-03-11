23d68d6fe82ee8e844e8b84060de8936db102a45
eduard <eduard42rus@mail.ru>
2024-03-11 19:50:05 +0700

    init commit

---
+++ src/main/java/io/smartlocker/MobilePayment/MobilePaymentApplication.java
@@ -0,0 +1,13 @@
+package io.smartlocker.MobilePayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilePaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/smartlocker/MobilePayment/MobilePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.smartlocker.MobilePayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
